465692abc3c8bb77171638ee2809c64296ae7b43
gsh06088@gmail.com <gsh06088@gmail.com>
2020-07-15 22:52:05 +0900

    오류 페이지

--- frontend/src/views/E404.vue
+++ frontend/src/views/E404.vue
@@ -1,6 +1,10 @@
 <template>
   <v-container fluid background-color="teal lighten-5" class="wrap-content">
-    <a>없는 주소 입니다.</a>
+    <v-col cols="12">
+      <v-img :src="require('../assets/E404.png')" class="my-3" contain height="200" />
+    </v-col>
+    <h2 id="select"> 찾으시는 페이지가 없습니다! </h2>
+    <a id="select" href="http://localhost:8080">메인으로 돌아가기</a>
   </v-container>
 </template>
 
@@ -40,6 +44,10 @@ li {
 }
 a {
   text-decoration: none;
+  text-align: center;
+}
+#select{
+  text-align: center;
 }
 .text_logo {
   color: black;

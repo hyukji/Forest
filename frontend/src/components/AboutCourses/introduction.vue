e0895443200ee725b0e1d8ea99e50bd0928f37e3
gsh06088@gmail.com <gsh06088@gmail.com>
2020-07-19 12:08:49 +0900

    강의 소개

--- frontend/src/components/AboutCourses/introduction.vue
+++ frontend/src/components/AboutCourses/introduction.vue
@@ -1,33 +1,24 @@
 <template>
   <div class="wrap-introduction">
-    <v-card class="ma-3" max-width="30%" outlined>
-      <v-container>
-        <v-row>
-          <v-col class="wrap-total-list" v-for="item in class_info" :key="item.title">
-            <v-list three-line>
-              <v-icon size="40pt" v-text="item.icon"></v-icon>
-              <v-list-item-title class="toal-list-title"  v-text="item.title"></v-list-item-title>
-              <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle>
-            </v-list>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card>
-    <p class="card-title"> 과목 소개 </p>
-    <p class="body"> 준비중입니다 </p>
-    <p class="card-title"> 수업 과정 </p>
-    <ul>
-      <li v-for="el of curriculum">
-        <v-icon
-          color="teal darken-2"
-          size="24"
-        >far fa-clipboard</v-icon>
-        <p class="li-title">{{el.list_title}}</p>
-      </li>
-    </ul>
-    <p>
-      <p class="card-title">추천 대상</p>
-      <v-card class="ma-3" max-width="30%" outlined >
+      <div class="introduction-half right">
+      <v-card class="mx-auto" max-width="70%">
+        <p class="card-title">개요</p>
+        <v-container>
+          <v-row>
+            <v-col class="wrap-total-list" v-for="item in class_info" :key="item.title">
+              <v-list three-line>
+                <v-icon size="40pt" v-text="item.icon"></v-icon>
+                <v-list-item-title class="toal-list-title"  v-text="item.title"></v-list-item-title>
+                <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle>
+              </v-list>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </div>
+    <div class="introduction-half left">
+      <v-card class="mx-auto" max-width="70%">
+        <p class="card-title">추천 대상</p>
         <v-container>
           <v-row>
             <v-col class="wrap-total-list" v-for="item in for_who" :key="item.title">
@@ -40,7 +31,22 @@
           </v-row>
         </v-container>
       </v-card>
-    </p>
+    </div>
+    <p class="card-title"> 과목 소개 </p>
+    <p class="body"> 준비중입니다 </p>
+    <p class="card-title"> 수업 과정 </p>
+    <ul>
+      <v-list-item v-for="item in curriculum" :key="item.title">
+        <v-list-item-avatar>
+          <v-icon size="20pt" color="gray">far fa-clipboard</v-icon>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title class="course_title" v-text="item.list_title"></v-list-item-title>
+          <v-list-item-content v-text="item.content"></v-list-item-content>
+        </v-list-item-content>
+      </v-list-item>
+    </ul>
+
   </div>
 </template>
 
@@ -53,7 +59,7 @@ export default {
     middle_title: "강의소개",
     class_info: [
       {
-        icon: "fas fa-code",
+        icon: "fab fa-python",
         title: "파이썬",
         subtitle: "프로그래밍 언어"
       },
@@ -67,21 +73,26 @@ export default {
       {
         icon: "fas fa-flushed",
         title: "코딩 입문자",
+        subtitle: "코딩 시작"
       },
       {
         icon: "fab fa-python",
-        title: "파이썬 개발자"
+        title: "파이썬 개발자",
+        subtitle: "파이썬 기초"
       }
     ],
     curriculum: [
       {
         list_title: "Course1",
+        content: "course1 내용입니다"
       },
       {
         list_title: "Course2",
+        content: "course2 내용입니다"
       },
       {
         list_title: "Course3",
+        content: "course3 내용입니다"
       }
     ],
   }),
@@ -130,6 +141,8 @@ export default {
   font-weight: 600;
   padding-top: 3%;
   padding-left: 6%;
+  padding-bottom: 3%;
+  border-bottom: 1px solid #d3d1d1;
 }
 .total-card {
   padding-top: 10%;
@@ -140,6 +153,16 @@ export default {
   padding-left: 3%;
   padding-top: 3%;
   padding-bottom: 2%;
+}
+.introduction-half {
+  display: inline-block;
+  width: 50%;
+  padding-left: 2%;
+  padding-right: 2%;
+}
+.course_title{
+  font-size: 1.2rem;
+  font-weight: 550;
 }
 .body{
   padding-left: 5%;

2607bc4b1bdc506ad331cc83b581b2958573338d
ddudy <dltnwl9908@naver.com>
2020-08-04 14:21:53 +0900

    dudu

---
+++ frontend/src/components/AboutCourses/Student_Care/lec_ass_list/lec1.vue
@@ -0,0 +1,149 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="information"
+    class="elevation-1"
+  >
+    <template slot="items" slot-scope="props">
+      <td>{{ props.item.name }}</td>
+      <td class="text-xs-right">{{ props.item.exam1 }}</td>
+      <td class="text-xs-right">{{ props.item.exam2 }}</td>
+      <td class="text-xs-right">{{ props.item.exam3 }}</td>
+      <td class="text-xs-right">{{ props.item.exma4 }}</td>
+      <td class="text-xs-right">{{ props.item.exam5 }}</td>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        headers: [
+          { text: '이 름 (학 번) ', align: 'left', value: 'name'},
+          { text: '문 제 1 ', value: 'exam1' },
+          { text: '문 제 2 ', value: 'exam2' },
+          { text: '문 제 3 ', value: 'exam3' },
+          { text: '문 제 4 ', value: 'exam4' },
+          { text: '문 제 5 ', value: 'exam5' }
+        ],
+        information: [
+          {
+            value: false,
+            name: '곽지혁 (201811001)',
+            exam1: 90,
+            exam2: 80,
+            exam3: 100,
+            exam4: 100,
+            exam5: 100
+          },
+          {
+            value: false,
+            name: '김태호 (201611023)',
+            exam1: 70,
+            exam2: 80,
+            exam3: 100,
+            exam4: 70,
+            exam5: 100
+          },
+          {
+            value: false,
+            name: '김태희 (201911032)',
+            exam1: 90,
+            exam2: 80,
+            exam3: 100,
+            exam4: 90,
+            exam5: 100
+          },
+          {
+            value: false,
+            name: '유재석 (201711100)',
+            exam1: 90,
+            exam2: 80,
+            exam3: 30,
+            exam4: 30,
+            exam5: 50
+          },
+          {
+            value: false,
+            name: '이수지 (201811111)',
+            exam1: 90,
+            exam2: 80,
+            exam3: 100,
+            exam4: 70,
+            exam5: 100
+          },
+          {
+            value: false,
+            name: '이예람 (201811122)',
+            exam1: 90,
+            exam2: 80,
+            exam3: 70,
+            exam4: 100,
+            exam5: 100
+          },
+          {
+            value: false,
+            name: '이효리 (201811133)',
+            exam1: 60,
+            exam2: 80,
+            exam3: 60,
+            exam4: 60,
+            exam5: 60
+          },
+          {
+            value: false,
+            name: '정슬인 (201811144)',
+            exam1: 90,
+            exam2: 80,
+            exam3: 100,
+            exam4: 100,
+            exam5: 100
+          },
+          {
+            value: false,
+            name: '정지훈 (201811188)',
+            exam1: 90,
+            exam2: 80,
+            exam3: 100,
+            exam4: 80,
+            exam5: 80
+          },
+          {
+            value: false,
+            name: '황광희 (202011200)',
+            exam1: 90,
+            exam2: 80,
+            exam3: 70,
+            exam4: 90,
+            exam5: 100
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+.search {
+  padding-top: 0;
+}
+.notice {
+  display: inline-block;
+  width: 79%;
+  padding-left: 5%;
+}
+.body-title {
+  font-size: 1.5rem;
+  font-weight: 600;
+  padding-left: 3%;
+
+  padding-top: 3%;
+
+  padding-bottom: 2%;
+}
+.table {
+  background-color: "#f9fcfc";
+}
+</style>
--- frontend/src/components/AboutCourses/Student_Care/student_care.vue
+++ frontend/src/components/AboutCourses/Student_Care/student_care.vue
@@ -1,3 +1,113 @@
 <template>
+  <div>
+  <row>
+  <div class="sidebar">
+  <v-card class="mx-auto" max-width="210">
+    <v-list>
+      <v-list-item
+      v-on:click="selectedComponent='student'"
+      >
+        <v-list-item-title>학생별</v-list-item-title>
+      </v-list-item>
 
+      <v-list-group>
+        <template v-slot:activator>
+          <v-list-item-title>수업 및 과제별</v-list-item-title>
+        </template>
+
+        <v-list-group
+          no-action
+          sub-group
+        >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>수업</v-list-item-title>
+            </v-list-item-content>
+          </template>
+
+          <v-list-item
+            v-for="(admin, i) in admins"
+            :key="i"
+            link
+            @click=""
+          >
+            <v-list-item-title v-text="admin[0]"></v-list-item-title>
+          </v-list-item>
+        </v-list-group>
+
+        <v-list-group
+          sub-group
+          no-action
+        >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>과제</v-list-item-title>
+            </v-list-item-content>
+          </template>
+          <v-list-item
+            v-for="(crud, i) in cruds"
+            :key="i"
+            link
+            @click=""
+          >
+            <v-list-item-title v-text="crud[0]"></v-list-item-title>
+          </v-list-item>
+        </v-list-group>
+      </v-list-group>
+
+      <v-list-item   v-on:click="selectedComponent='lec_ass'">
+        <v-list-item-title>통계</v-list-item-title>
+      </v-list-item>
+
+    </v-list>
+  </v-card>
+</div>
+</row>
+<row>
+<keep-alive>
+  <component v-bind:is="selectedComponent">
+    <p> Default content </p>
+  </component>
+</keep-alive>
+</row>
+
+</div>
 </template>
+
+<script>
+import student from '@/components/AboutCourses/Student_Care/student';
+import lec_ass from '@/components/AboutCourses/Student_Care/lec_ass_list/lec1';
+import statistics from '@/components/AboutCourses/Student_Care/statistics';
+
+
+  export default {
+    data: function() {
+      return{
+            selectedComponent: 'student'
+            }
+      },
+    components : {
+      student, lec_ass, statistics
+    }
+};
+</script>
+
+<style scoped>
+.sidebar {
+  padding-top: 3%;
+  float: left;
+  width: 20%;
+  padding-left: 2%;
+}
+.side_title {
+  font-size: 1.15rem;
+  font-weight: 700;
+}
+.wrap-side-list {
+  border-top: 2px solid #8d8d8d;
+}
+.side-list-title {
+  margin-top: auto;
+  margin-bottom: auto;
+}
+</style>

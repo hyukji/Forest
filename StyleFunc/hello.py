4c7a2d701a6b4db09f71f6d2eb969fd585855787
gsh06088@gmail.com <gsh06088@gmail.com>
2020-09-16 02:46:15 +0900

    naming

--- StyleFunc/hello.py
+++ StyleFunc/hello.py
@@ -9,3 +9,20 @@ aa = 60
 
 bb = 1 if (aa <= 0) else aa
 print(f'{aa} {bb}')  # aa=0 bb=1
+
+class ILoveMusic:
+    return 1
+class iLovemusic:
+    def _music:
+        return 0
+    _math=0
+    math=1
+class i_love_music3:
+    return 1
+
+def ILoveMath:
+    return 1
+def iLovemath:
+    return 1
+def i_love_math:
+    return 1

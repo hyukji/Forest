b5242b17e5c68511e17f7de6cdb02e0e786f29fd
hyukji <wlgur0416@dgist.ac.kr>
2020-09-16 16:32:49 +0900

    crowling and func update

--- StyleFunc/hello.py
+++ StyleFunc/hello.py
@@ -1,11 +1,224 @@
-aa = 60
-bb = 1 if (aa <= 0) else aa
-print(f'{aa} {bb}')  # aa=0 bb=1
+map=[[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+          [ 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
+          [ 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1 ],
+          [ 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1 ],
+          [ 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1 ],
+          [ 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1 ],
+          [ 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1 ],
+          [ 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1 ],
+          [ 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1 ],
+          [ 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1 ],
+          [ 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1 ],
+          [ 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 ],
+          [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+          [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 ] ]
+m1=[ [ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+          [ 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
+          [ 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1 ],
+          [ 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1 ],
+          [ 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1 ],
+          [ 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1 ],
+          [ 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1 ],
+          [ 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1 ],
+          [ 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1 ],
+          [ 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1 ],
+          [ 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1 ],
+          [ 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+          [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+          [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 ] ]
+m2= [[ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+          [ 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
+          [ 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1 ],
+          [ 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1 ],
+          [ 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1 ],
+          [ 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1 ],
+          [ 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1 ],
+          [ 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1 ],
+          [ 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1 ],
+          [ 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1 ],
+          [ 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1 ],
+          [ 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 ],
+          [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 ],
+          [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 ] ]
+m3=[ [ 0, 1, 1, 1, 1, 1],
+          [ 0, 0, 0, 0, 0, 1 ],
+          [ 1, 0, 0, 0, 0, 1 ],
+          [ 1, 0, 0, 0, 0, 1 ],
+          [ 1, 0, 0, 0, 0, 1 ],
+          [ 1, 1, 1, 1, 0, 0 ],]
 
-if(aa == 0):
-    print()
+m4=[ [ 0, 1, 1, 1, 1, 1],
+          [ 0, 0, 0, 0, 0, 1 ],
+          [ 1, 0, 1, 1, 1, 1 ],
+          [ 1, 0, 0, 0, 0, 1 ],
+          [ 1, 0, 1, 1, 0, 1 ],
+          [ 1, 1, 1, 1, 0, 0 ],]
+#이승우 학생의 도움을 받았습니다.
+class Player:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+    def move(self, dir):
+        if dir =='up':
+            self.y += 1
+        elif dir =='down':
+            self.y -= 1
+        elif dir =='left':
+            self.x -= 1
+        elif dir =='right':
+            self.x += 1
+        pass
+    def get_position(self):
+        return (self.x, self.y)
 
-aa = 60
+class MazeGame:
+    def __init__(self, map=None):
+        self.map = map
+        self.player = Player()
+        self.path = []
 
-bb = 1 if (aa <= 0) else aa
-print(f'{aa} {bb}')  # aa=0 bb=1
+    def play(self):
+        Map = self.map
+        Map.append([])
+        for j in range(len(Map[0])):
+            Map[-1].append(1)
+        for i in range(len(Map)):
+            Map[i].append(1)
+        direction = 2
+        position = [0, 0]
+        self.path.append((0, 0))
+        while position != [len(Map[0])-2, len(Map)-2]:
+            if direction == 2:
+                if Map[position[1]][position[0] + 1] == 0:
+                    if position[0] + 1 < len(Map[0]):
+                        direction = 1
+                        position[0] = position[0] + 1
+                        self.path.append(tuple(position))
+                    else:
+                        pass
+                else:
+                    if Map[position[1] + 1][position[0]] == 0:
+                        if position[1] + 1 < len(Map[1]):
+                            position[1] = position[1] + 1
+                            self.path.append(tuple(position))
+                        else:
+                            pass
+                    else:
+                        if Map[position[1]][position[0] - 1] == 0:
+                            if position[0] - 1 >= 0:
+                                direction = 3
+                                position[0] = position[0] - 1
+                                self.path.append(tuple(position))
+                            else:
+                                pass
+                        else:
+                            if position[1] - 1 >= 0:
+                                direction = 0
+                                position[1] = position[1] - 1
+                                self.path.append(tuple(position))
+                            else:
+                                pass
+            elif direction == 3:
+                if Map[position[1] + 1][position[0]] == 0:
+                    if position[1] + 1 < len(Map[1]):
+                        direction = 2
+                        position[1] = position[1] + 1
+                        self.path.append(tuple(position))
+                    else:
+                        pass
+                else:
+                    if Map[position[1]][position[0] - 1] == 0:
+                        if position[0] - 1 >= 0:
+                            position[0] = position[0] - 1
+                            self.path.append(tuple(position))
+                        else:
+                            pass
+                    else:
+                        if Map[position[1] - 1][position[0]] == 0:
+                            if position[1] - 1 >= 0:
+                                direction = 0
+                                position[1] = position[1] - 1
+                                self.path.append(tuple(position))
+                            else:
+                                pass
+                        else:
+                            if position[0] + 1 < len(Map[0]):
+                                direction = 1
+                                position[0] = position[0] + 1
+                                self.path.append(tuple(position))
+                            else:
+                                pass
+            elif direction == 0:
+                if Map[position[1]][position[0] - 1] == 0:
+                    if position[0] - 1 >= 0:
+                        direction = 3
+                        position[0] = position[0] - 1
+                        self.path.append(tuple(position))
+                    else:
+                        pass
+                else:
+                    if Map[position[1] - 1][position[0]] == 0:
+                        if position[1] - 1 >= 0:
+                            position[1] = position[1] - 1
+                            self.path.append(tuple(position))
+                        else:
+                            pass
+                    else:
+                        if Map[position[1]][position[0] + 1] == 0:
+                            if position[0] + 1 < len(Map[0]):
+                                direction = 1
+                                position[0] = position[0] + 1
+                                self.path.append(tuple(position))
+                            else:
+                                pass
+                        else:
+                            if position[1] + 1 < len(Map[1]):
+                                direction = 2
+                                position[1] = position[1] + 1
+                                self.path.append(tuple(position))
+                            else:
+                                pass
+            elif direction == 1:
+                if Map[position[1] - 1][position[0]] == 0:
+                    if position[1] - 1 >= 0:
+                        direction = 0
+                        position[1] = position[1] - 1
+                        self.path.append(tuple(position))
+                    else:
+                        pass
+                else:
+                    if Map[position[1]][position[0] + 1] == 0:
+                        if position[0] + 1 < len(Map[0]):
+                            position[0] = position[0] + 1
+                            self.path.append(tuple(position))
+                        else:
+                            pass
+                    else:
+                        if Map[position[1] + 1][position[0]] == 0:
+                            if position[1] + 1 < len(Map[1]):
+                                direction = 2
+                                position[1] = position[1] + 1
+                                self.path.append(tuple(position))
+                            else:
+                                pass
+                        else:
+                            if position[0] - 1 >= 0:
+                                direction = 3
+                                position[0] = position[0] - 1
+                                self.path.append(tuple(position))
+                            else:
+                                pass
+        for i in range(len(Map)):
+            del Map[i][-1]
+        del Map[-1]
+
+    def get_map(self):
+        return self.map
+    def get_path(self):
+        return self.path
+
+if __name__ == "__main__" :
+    game1 = MazeGame(m1)
+    game1.play()
+    print(game1.get_map())
+    print(game1.get_path())

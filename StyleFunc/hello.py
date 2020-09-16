fed044dc1045b76d92bda193b68cb0f57d5ef17b
ddudy <dltnwl9908@naver.com>
2020-09-16 19:11:02 +0900

    suji.py

--- StyleFunc/hello.py
+++ StyleFunc/hello.py
@@ -2,10 +2,11 @@ aa = 60
 bb = 1 if (aa <= 0) else aa
 print(f'{aa} {bb}')  # aa=0 bb=1
 
+
 if(aa == 0):
     print()
 
-aa = 60
+aa = 70
 
 bb = 1 if (aa <= 0) else aa
 print(f'{aa} {bb}')  # aa=0 bb=1
